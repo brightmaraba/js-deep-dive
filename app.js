18bf3bcb6ba78c343d46b650d27b8e31bed779c7
Brian Koech <brightkoech@gmail.com>
2021-03-25 05:57:00 +0800

    Completed comments

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // I woke up early today.
 // Statements - are a set of instructions
-// comments - shortcut CTRL + K
+// comments - shortcut CTRL + /
+
+// console.log examples
+/*
+console.log("hello world");
+console.log("hello africa");
+console.log("hello australia");
+*/
+
+document.write("hello, Anna");
